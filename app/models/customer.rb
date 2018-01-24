3e3e1b021b4d83484ee1ed4df1e622b064f8db94
Yuki Takahashi <yuki0627@gmail.com>
2018-01-24 20:18:55 +0900

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,9 +2,24 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :omniauthable, omniauth_providers: %i(google)
 
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def self.find_for_google(auth)
+    customer = Customer.find_by(email: auth.info.email)
+  
+    unless customer
+      customer = Customer.new(email: auth.info.email,
+                      provider: auth.provider,
+                      uid:      auth.uid,
+                      password: Devise.friendly_token[0, 20],
+                                   )
+    end
+    customer.save
+    customer
   end
 end
